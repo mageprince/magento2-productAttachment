fbea94c0ef684302cf0c5c251f7ac3b016c61e39
prince <mail.mageprince@gmail.com>
2018-03-06 23:36:42 +0530

    Copyright license update

--- Controller/Adminhtml/Index/Edit.php
+++ Controller/Adminhtml/Index/Edit.php
@@ -1,5 +1,31 @@
 <?php
 
+/**
+ * MagePrince
+ * Copyright (C) 2018 Mageprince
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html
+ *
+ * @category MagePrince
+ * @package Prince_Productattach
+ * @copyright Copyright (c) 2018 MagePrince
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author MagePrince
+ */
+
 namespace Prince\Productattach\Controller\Adminhtml\Index;
 
 use Magento\Backend\App\Action;
